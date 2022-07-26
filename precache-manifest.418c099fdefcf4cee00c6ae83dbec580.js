4eaf2115714840cf119733605340f8dbffa3212c
beldeeb <bassel_eldeeb@knights.ucf.edu>
2022-07-26 16:24:35 -0400

    Updates

---
+++ precache-manifest.418c099fdefcf4cee00c6ae83dbec580.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e72ab97e1c3518aeac60",
+    "url": "/sweatcheck-large/static/css/main.523a9466.chunk.css"
+  },
+  {
+    "revision": "e72ab97e1c3518aeac60",
+    "url": "/sweatcheck-large/static/js/main.0ea8a3fc.chunk.js"
+  },
+  {
+    "revision": "16d753c778ede722af73",
+    "url": "/sweatcheck-large/static/js/runtime~main.d809caaa.js"
+  },
+  {
+    "revision": "c0757af1a4504415f37a",
+    "url": "/sweatcheck-large/static/css/2.be098504.chunk.css"
+  },
+  {
+    "revision": "c0757af1a4504415f37a",
+    "url": "/sweatcheck-large/static/js/2.c06c37eb.chunk.js"
+  },
+  {
+    "revision": "bbcc662a58e1aed83b10fbbdea98bb23",
+    "url": "/sweatcheck-large/static/media/logo.bbcc662a.png"
+  },
+  {
+    "revision": "6b048deeb4c93340bef73e90dde1c989",
+    "url": "/sweatcheck-large/static/media/bg2.6b048dee.png"
+  },
+  {
+    "revision": "8c36e38fbbd51243865524bcefca3cad",
+    "url": "/sweatcheck-large/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/sweatcheck-large/precache-manifest.418c099fdefcf4cee00c6ae83dbec580.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/sweatcheck-large/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
